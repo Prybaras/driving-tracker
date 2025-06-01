5835b2251ae84311abb82caa668734255c13cf29
Guled <guled@example.com>
2025-06-01 20:06:51 +0200

    voeg basisvariabelen en functies toe voor datum en opslag

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-document.querySelectorAll;
+const goal = 120;
+let currentDate = new Date();
+let selectedDate = getTodaykey(); // fout: kleine letter 'k'
+
+function getTodayKey() {
+  return new Date().toISOString().split("T")[0];
+}
+
+function getData() {
+  JSON.parse(localStorage.getItem("driveData") || "{}"); // fout: mist 'return'
+}
